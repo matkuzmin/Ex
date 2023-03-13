abd881c2cb92f7b69ff608e67cfbbd8fd510b6a0
matvey <qjlf@inbox.ru>
2023-03-13 12:44:30 +0300

    решение задачи

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 
 public class Main {
     public static void main(String[] args) {
+        String s = "1234567";
+        if (s.length() % 2 == 0) {
+            String s1 = s.substring(0, s.length() / 2);
+            String s2 = s.substring(s.length() / 2, s.length());
+            System.out.println("Строка 1 " + s1 + " Строка 2 " + s2);
+        }
+        else
+            System.out.println("Введите строку в которой колличество символов четное");
 
     }
 
